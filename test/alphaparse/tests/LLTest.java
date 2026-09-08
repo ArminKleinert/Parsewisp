e6294906b054d247c8350b9d440986c51396abe7
ArminKleinert <armin.kleinert@t-online.de>
2026-09-08 22:58:54 +0200

    .

--- test/alphaparse/tests/LLTest.java
+++ test/alphaparse/tests/LLTest.java
@@ -8,29 +8,39 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 /*
-OLD:
-.pDirect____ : 818.3564290666667
-.pLeftRecH__ : 5648.402744588889
-.pLeftRec___ : 93.51533295555556
-.pRightRecEH : 5422.940529233334
-.pRightRecE_ : 166.97516004444444
-.pRightRecH_ : 5269.696173433334
-.pRightRec__ : 154.55672841111112
-.pRegex_____ : 0.09188858888888889
+Array-based:
+.pDirect____ : 734.5994077222223
+.pLeftRecH__ : 5513.966410888888
+.pLeftRec___ : 89.11456878888889
+.pRightRecEH : 5935.430014133333
+.pRightRecE_ : 168.37849315555556
+.pRightRecH_ : 5533.560816744444
+.pRightRec__ : 160.0001591333333
+.pRegex_____ : 0.1617999
 
-NEW:
-.pDirect____ : 25.095488355555556
-.pLeftRecH__ : 5464.817504477778
-.pLeftRec___ : 88.95493266666666
-.pRightRecEH : 5563.318510811111
-.pRightRecE_ : 168.4477270111111
-.pRightRecH_ : 5544.912572888889
-.pRightRec__ : 154.1189783111111
-.pRegex_____ : 0.07210718888888888
+Array+Marker:
+.pDirect____ : 24.482446644444444
+.pLeftRecH__ : 5373.383967522222
+.pLeftRec___ : 98.55333840000002
+.pRightRecEH : 5385.678086377778
+.pRightRecE_ : 172.39012652222223
+.pRightRecH_ : 5309.490438144445
+.pRightRec__ : 157.92169719999998
+.pRegex_____ : 0.1264512111111111
+
+Linked:
+.pDirect____ : 22.615405199999998
+.pLeftRecH__ : 5353.148949677778
+.pLeftRec___ : 89.34735326666667
+.pRightRecEH : 5850.970126155556
+.pRightRecE_ : 174.1519648
+.pRightRecH_ : 5599.451224055556
+.pRightRec__ : 156.4384317777778
+.pRegex_____ : 0.1361259
  */
 
 class LLTest {
-    final int reps = 30_000;
+    final int reps = 300;
     final int runsPerMeasure = 100;
     final String s = "b" + "a".repeat(reps);
     final String sReduced = "b" + "a".repeat(reps / 10);
