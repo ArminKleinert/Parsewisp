5c22c7f48fd4ff92113870a2a03addb060336416
ArminKleinert <armin.kleinert@t-online.de>
2026-09-23 22:58:19 +0200

    .

--- test/de/kleinert/parsewisp/tests/typical/CornerCasesTests.java
+++ test/de/kleinert/parsewisp/tests/typical/CornerCasesTests.java
@@ -5,6 +5,9 @@ import de.kleinert.parsewisp.testutil.PT;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.HashSet;
+import java.util.Set;
+
 class CornerCasesTests {
     @Test
     void test10() {
@@ -21,5 +24,26 @@ class CornerCasesTests {
         Assertions.assertEquals(
                 PT.create("S", PT.create("E")),
                 Parsewisp.parser("S = E+\nE = eps").parse(""));
+    }
+
+    @Test
+    void test11() {
+        Assertions.assertEquals(
+                PT.create("S", "a", PT.create("E"), "b"),
+                Parsewisp.parser("S = 'a' 1*3 E 'b'\nE = eps").parse("ab"));
+        Assertions.assertEquals(
+                PT.create("S", "a", PT.create("E"), PT.create("E"), "b"),
+                Parsewisp.parser("S = 'a' 2*3 E 'b'\nE = eps").parse("ab"));
+
+        Assertions.assertEquals(
+                Set.of(PT.create("S", "a", PT.create("E"), "b"),
+                        PT.create("S", "a", PT.create("E"), PT.create("E"), "b"),
+                        PT.create("S", "a", PT.create("E"), PT.create("E"), PT.create("E"), "b")),
+                new HashSet<>(Parsewisp.parser("S = 'a' 1*3 E 'b'\nE = eps").parses("ab")));
+    }
+
+    @Test
+    void test12() {var p = Parsewisp.parser("S = 2*4 E 'a'\nE = eps");
+        System.out.println(p.parse("a"));
     }
 }
